ec2c22fd519d0b2a10217483c1dac423bfac019d
petrivanov <krest2005@yandex.ru>
2021-05-06 13:16:47 +0300

    remove timers from home page

--- src/components/CampaignWidget.vue
+++ src/components/CampaignWidget.vue
@@ -14,17 +14,15 @@
               <h2 class="fs-2 text-dark mb-3 display-6 lh-1 fw-bold">Campaign {{campaign.id}}</h2>
             </router-link>
           </div>
-          <div v-if="!finished" class="row">
+          <div class="row">
             <div class="col">
-              <span v-if="inProgress" class="fw-bold">Ends in</span>
-              <span v-else class="fw-bold">Starts in</span>
+              <span class="fw-bold">Start: {{formatTime(startDate)}}</span>
             </div>
           </div>
-          <div v-if="!finished" class="row text-dark text-center">
-            <div class="col countdown-section"><span>{{days}}</span>days</div>
-            <div class="col countdown-section"><span>{{hours}}</span>Hours</div>
-            <div class="col countdown-section"><span>{{minutes}}</span>Minutes</div>
-            <div class="col countdown-section"><span>{{seconds}}</span>Seconds</div>
+          <div class="ro">
+            <div class="col">
+              <span class="fw-bold">Finish: {{formatTime(finishDate)}}</span>
+            </div>
           </div>
           <div class="row">
             <div class="col">
@@ -73,27 +71,13 @@ export default defineComponent({
       return props.campaign.funded.div(props.campaign.target.div(100)).toNumber()
     })
 
-    let days = ref(0), hours = ref(0), minutes = ref(0), seconds = ref(0)
-    setInterval(() => {
-      const now = moment().unix()
-      let campaignDatesDiff
-      if (now <= props.campaign.startedAt.unix()){
-        campaignDatesDiff = props.campaign.startedAt.diff(moment())
-      } else {
-        campaignDatesDiff = props.campaign.finishedAt.diff(moment())
-      }
-      let duration
-      if (campaignDatesDiff > 0) {
-        duration = moment.duration(campaignDatesDiff)
-      } else {
-        duration = moment.duration(0)
-      }
-      days.value = duration.days()
-      hours.value = duration.hours()
-      minutes.value = duration.minutes()
-      seconds.value = duration.seconds()
+    const startDate = computed(() => {
+      return moment(props.campaign.startedAt)
+    })
 
-    }, 1000)
+    const finishDate = computed(() => {
+      return moment(props.campaign.finishedAt)
+    })
 
     const inProgress = computed(() => {
       const now = moment().unix()
@@ -109,20 +93,24 @@ export default defineComponent({
       return props.campaign.target.lte(props.campaign.funded)
     })
 
+    const formatTime = (time: moment.Moment): string => {
+      return time.format("DD.MM.YYYY HH:mm")
+    }
+
 
     return {
       targetEthers,
       fundedEthers,
       fundedPercentage,
 
-      days,
-      hours,
-      minutes,
-      seconds,
-
       inProgress,
       finished,
-      succeed
+      succeed,
+
+      startDate,
+      finishDate,
+
+      formatTime
     }
   }
 })
